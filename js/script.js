9684a6830c178bad1a7fefed2ecec6138df9069e
lalit-machra <lalitmachra0403@gmail.com>
2024-08-15 23:59:02 +0530

    Handle passwords not matching

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+
+password.addEventListener("input", () => {
+  if (password.value !== confirmPassword.value) {
+    password.setCustomValidity("Invalid field");
+    confirmPassword.setCustomValidity(" ");
+  }
+  else {
+    password.setCustomValidity("");
+    confirmPassword.setCustomValidity("");
+  }
+})
+
+confirmPassword.addEventListener("input", () => {
+  if (password.value !== confirmPassword.value) {
+    password.setCustomValidity("Invalid field");
+    confirmPassword.setCustomValidity(" ");
+  }
+  else {
+    password.setCustomValidity("");
+    confirmPassword.setCustomValidity("");
+  }
+})
